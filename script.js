bf25a5f1e7cece6a4d04b6a1a24b3fb5dbac6794
Viktor <tsukanov87@mail.ru>
2022-09-20 21:28:23 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,34 @@
+console.log("Hello");
+console.log("world");
+console.log("3333");
+console.log(334);
+
+
+console.log('Hello' + "world");//конкотенация
+console.log("Hello" + " " + "Hello");
+
+
+console.info("hello");
+
+//alert("hello")
+
+
+document.getElementById("out").innerHTML = 2222;// обращаемся к элементу по id
+document.getElementById("out").innerHTML = "hello";
+
+document.getElementById("out").innerHTML = "<b>2020</b>";
+
+
+document.querySelector("h2.header").innerHTML = "Точно заголовок 2-го уровня?"
+
+
+document.querySelector("#one").innerHTML = 777;
+
+document.getElementById("one").innerHTML = 999;
+
+
+let a;
+a = document.querySelector("#one")// внутрь а получили параграф
+
+a.innerHTML = 2323;
+
